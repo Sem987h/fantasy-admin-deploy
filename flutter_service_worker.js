79f5e3b58ec829117b7778e207b14f60c074038e
Hemang <hemanggoswami987@gmail.com>
2024-01-04 21:30:57 +0530

    Deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "d6db2c741575a520c4073f70cf04e3a3",
-"index.html": "a03bf0808780e7036904d3682a8db8e1",
-"/": "a03bf0808780e7036904d3682a8db8e1",
-"main.dart.js": "f33bd2e94dba9cde2d503b2f9cd18a3d",
+"index.html": "6f6bd4b4a301cbe394d5eb260ca2c726",
+"/": "6f6bd4b4a301cbe394d5eb260ca2c726",
+"main.dart.js": "cab588898139689cfc4e99ec6b94071b",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "1de36cf62f27b05827942f7d6f9cf7a4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
